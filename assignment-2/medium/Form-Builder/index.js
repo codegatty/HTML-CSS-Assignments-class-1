8aa8b62b0ca13a038d2fbebf0ed26d57d5f01eaa
codegatty <rajithgatty61@gmail.com>
2026-02-09 07:46:40 +0530

    --updated form-builder

--- assignment-2/medium/Form-Builder/index.js
+++ assignment-2/medium/Form-Builder/index.js
@@ -1,7 +1,41 @@
-function main(){
-    const selectedType=document.getElementById(selectedType)
-    console.log
+function main() {
+  const selectedTypeEle = document.getElementById("selected-type");
+  const submitBtnEle = document.getElementById("submit");
+  const selectedTypeLabelTextEle = document.getElementById(
+    "selected-type-label-text",
+  );
+  const formPreviewEle = document.getElementById("form-preview");
 
+  submitBtnEle.addEventListener("click", (event) => {
+    event.preventDefault();
+    const selectedType = selectedTypeEle.value;
+    const labelText = selectedTypeLabelTextEle.value;
+
+    const formElement = createDynamicFormComponent(selectedType, labelText);
+    console.log(formElement);
+    formPreviewEle.appendChild(formElement);
+  });
+}
+
+function createDynamicFormComponent(inputType, labelText) {
+  const containerDivEle = document.createElement("div");
+  const inputEle = document.createElement("input");
+  const labelEle = document.createElement("label");
+
+  inputEle.setAttribute("type", inputType);
+
+  labelEle.innerText = labelText;
+
+  if (inputType !== "button") {
+    containerDivEle.appendChild(labelEle);
+    containerDivEle.appendChild(inputEle);
+  }
+  if (inputType === "button") {
+    inputEle.setAttribute("value", labelText);
+    containerDivEle.appendChild(inputEle);
+  }
+
+  return containerDivEle;
 }
 
 main();
